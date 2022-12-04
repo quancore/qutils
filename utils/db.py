0700700eb491bfdd2ed129e634551faf5c6f927f
Baran Nama <bn@data-zoo.de>
2022-12-04 19:32:27 +0100

    fly deployment

--- utils/db.py
+++ utils/db.py
@@ -522,11 +522,6 @@ class Table(metaclass=TableMeta):
             if old_init is not None:
                 await old_init(con)
 
-        # ctx = ssl.create_default_context(cafile='./rds-combined-ca-bundle.pem')
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
-        #cls._pool = pool = await asyncpg.create_pool(uri, ssl=ctx, init=init, **kwargs)
         cls._pool = pool = await asyncpg.create_pool(uri, init=init, **kwargs)
 
 
